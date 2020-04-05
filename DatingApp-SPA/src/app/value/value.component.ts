77c661c00d6d85af9f32098e5b8f29a7e416df79
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-04-05 15:20:40 +0200

    Display data from API and add bootstrap

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -22,7 +22,7 @@ export class ValueComponent implements OnInit {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
     }, error => {
-      console.log(error)
+      console.log(error);
     });
   }
 
