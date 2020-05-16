816a8ccabe3a3a7e3d6a08828639a6da467ca6d0
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-05-16 15:10:15 +0200

    Using routing in nav components

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,9 +12,10 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+    private router: Router) {
 
-   }
+  }
 
   ngOnInit() {
   }
@@ -21,20 +23,25 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-      this.alertify.succes('Logged in succesfully');
-    },
-    error => {
-      this.alertify.error(error);
+        this.alertify.succes('Logged in succesfully');
+      },
+      error => {
+        this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       }
     );
   }
 
   loggedIn() {
-   return this.authService.loggedIn();
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+
+    //adding navigation option
+    this.router.navigate(['/home']);
   }
 }
