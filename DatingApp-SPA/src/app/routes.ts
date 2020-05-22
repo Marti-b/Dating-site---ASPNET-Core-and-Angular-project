ede35e10e4100d051b0868817eb3d94d81699b37
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-05-22 18:07:43 +0200

    Design template of member profile edit page

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -20,7 +20,11 @@ export const appRoutes: Routes = [
       { path: 'members', component: MemberListComponent, resolve: {users:MemberListResolver} },
       { path: 'members/:id', component: MemberDetailComponent,
             resolve: {user: MemberDetailResolver} },
-      {path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}},
+            {
+              path: 'member/edit',
+              component: MemberEditComponent,
+              resolve: {user: MemberEditResolver}
+            },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent },
     ]
