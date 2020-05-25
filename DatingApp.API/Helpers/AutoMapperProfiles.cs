6cc8e3fd0b565a965465d114af24ee709844ec4c
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-05-25 10:33:12 +0200

    Add controller and dto to update member's detail
    Add UserForUpdateDto, HTTP Put contorller

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -20,8 +20,10 @@ namespace DatingApp.API.Helpers
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt =>
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
-
+         
             CreateMap<Photo, PhotosForDetailedDto>();
+
+               CreateMap<UserForUpdateDto, User>();
 
         }
     }
