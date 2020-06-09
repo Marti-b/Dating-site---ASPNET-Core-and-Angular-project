e875118296bfd44613985bcc080df9803c3573c7
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-06-09 10:30:54 +0200

    Add file uploading (3rd party)
    Add ng2 file upload

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import {NgxGalleryModule} from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -30,6 +31,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+
 
 
 export function tokenGetter() {
@@ -67,6 +69,7 @@ export function tokenGetter() {
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
+    FileUploadModule,
     JwtModule.forRoot(
       {
         config: {
--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from 'src/app/_models/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,9 +13,30 @@ export class PhotoEditorComponent implements OnInit {
 
   @Input() photos: Photo[];
 
-  constructor() { }
+  uploader: FileUploader;
+  hasBaseDropZoneOver: false;
+  baseUrl = environment;
+
+  response: string;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
   }
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+  initializeUploader() {
+    this.uploader = new FileUploader(
+      {
+        url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+        authToken: 'Bearer ' + localStorage.getItem('token'),
+        isHTML5: true,
+        allowedFileType: ['image'],
+        removeAfterUpload: true,
+        autoUpload: false,
+        maxFileSize: 10 * 1024 * 1024
 
+      });
+  }
 }
