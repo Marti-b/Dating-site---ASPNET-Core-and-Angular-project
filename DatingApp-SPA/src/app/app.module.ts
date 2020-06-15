cdc5b1649c72ee9f66efdd0fadfb3b2d31c50265
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-06-15 17:48:32 +0200

    Add Reactive Forms to Register comp
    Create registerForm using FormGroup class

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule, Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
@@ -64,6 +64,7 @@ export function tokenGetter() {
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,21 +13,28 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService ) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl(),
+        password: new FormControl(),
+        confirmPassword: new FormControl()
+      });
   }
 
-  register(){
-   this.authService.register(this.model).subscribe(() =>{
-    this.alertify.success('registration succesful');
-   }, error => {
-    this.alertify.error(error)
-   }
-   )
+  register() {
+   /* this.authService.register(this.model).subscribe(() => {
+      this.alertify.success('registration succesful');
+    }, error => {
+      this.alertify.error(error);
+    }); */
+    console.log(this.registerForm.value);
   }
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
     console.log('cancelled');
   }
