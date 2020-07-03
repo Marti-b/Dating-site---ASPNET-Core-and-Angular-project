3cbd75ba5de3968c74efc1ee5d3a13c7e886c62c
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-07-03 20:29:10 +0200

    Add helper classes for Paging

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
 namespace DatingApp.API.Helpers
 {
     public static class Extensions
@@ -8,15 +10,21 @@ namespace DatingApp.API.Helpers
         {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "AddApplicationError");
-             response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
 
-
+        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
 
-            if(theDateTime.AddYears(age)> DateTime.Today){
+            if (theDateTime.AddYears(age) > DateTime.Today)
+            {
                 age--;
             }
             return age;
