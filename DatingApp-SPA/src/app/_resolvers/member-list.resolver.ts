888dcaaaaf5f700f391b5edac06cbf140ebc8cb7
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-07-05 14:35:54 +0200

    Setting up pagination in the SPA
    Adding Pagination interface, PaginatedResult<T>
    
    Changing getUsers method retriveing PaginatedResult<User[]>

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,16 +8,18 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private userService: UserService, private router: Router,
-    private alertify: AlertifyService) {}
+    private alertify: AlertifyService) { }
 
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers().pipe(
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
       catchError(error => {
         this.alertify.error('Problem retrieving data');
         this.router.navigate(['/home']);
-        return of (null);
+        return of(null);
       })
     );
   }
