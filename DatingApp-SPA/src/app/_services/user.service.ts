1d1056154c726416603ee5759b78125a3f4dfdb6
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-07-09 11:27:44 +0200

    Add Send like functionality to SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -56,7 +56,12 @@ export class UserService {
 
     return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
   }
+
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
+
+  sendLike(id: number, recipientId: number){
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
   }
 }
