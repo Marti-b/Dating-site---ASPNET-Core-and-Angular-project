e56329cc2513a5be2b2b5312c70cf8341649cb6c
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-09-01 17:15:33 +0200

    Adding Query params to Angular route (taking to messages)
    Using query strings to get to messages when clicking on Messages icons in the app
    
    [queryParams] - set ups which tab opens when we get to the user

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,7 +12,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-
+  @ViewChild('memberTabs', { static: true }) memberTabs: TabsetComponent;
   user: User;
 
   galleryOptions: NgxGalleryOptions[];
@@ -26,6 +27,10 @@ export class MemberDetailComponent implements OnInit {
       this.user = data.user;
     });
 
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    })
 
     this.galleryOptions = [
       {
@@ -52,11 +57,15 @@ export class MemberDetailComponent implements OnInit {
     }
     return imageUrls;
   }
- /* loadUser() {
-    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-      this.user = user;
-    }, error => {
-      this.alertify.error(error);
-    });
-  } */
+  /* loadUser() {
+     this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+       this.user = user;
+     }, error => {
+       this.alertify.error(error);
+     });
+   } */
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
 }
