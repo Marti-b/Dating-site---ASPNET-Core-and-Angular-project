0159464c2cd779783f77e110c455a1267ed7b86d
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-09-04 01:40:37 +0200

    Fixing error: register form is vshown after logging in

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -19,5 +20,9 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(registerMode: boolean){
     this.registerMode = registerMode;
+  }
+  loggedIn()
+  {
+    return this.authService.loggedIn();
   }
 }
